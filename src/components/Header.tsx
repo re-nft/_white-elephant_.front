f13b571932236b4eef0d4eb6f6a4f964eac7e3e8
nazariyv <nazariy@inbox.ru>
2020-12-26 01:06:59 +0000

    [+] added the socials

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import { Box } from "@material-ui/core";
 import { Telegram, Twitter } from "@material-ui/icons";
 
+import Discord from "../public/img/discord.svg";
+import Medium from "../public/img/medium.svg";
+
 export default function Header() {
+  console.error(Medium);
   return (
     <Box style={{ position: "absolute", top: "0", right: "0" }}>
       <Box style={{ margin: "2em" }}>
@@ -14,6 +18,22 @@ export default function Header() {
           }}
         >
           <Box>We love to stick around the cool kids:</Box>
+          <a
+            style={{ height: "25px", width: "25px" }}
+            href="https://discord.gg/tyqm8t4huz"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={Discord} />
+          </a>
+          <a
+            style={{ height: "25px", width: "25px" }}
+            href="https://medium.com/renftlabs"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={Medium} />
+          </a>
           <a
             href="https://t.me/renftlabs"
             target="_blank"
