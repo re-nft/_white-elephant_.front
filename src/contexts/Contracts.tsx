64c511454c9b19529f37bfe3562dfe9234ed24c1
naz <nazariy@inbox.ru>
2021-01-01 19:48:35 +0000

    +

--- src/contexts/Contracts.tsx
+++ src/contexts/Contracts.tsx
@@ -7,10 +7,10 @@ import React, {
 } from "react";
 import { ethers } from "ethers";
 
-import DappContext from "./Dapp";
-import { abis as _abis } from "../contracts";
 import { Game } from "../typechain/Game";
 import { GameFactory } from "../typechain/GameFactory";
+import DappContext from "./Dapp";
+import { abis as _abis } from "../contracts";
 
 type Optional<T> = T | undefined;
 type Contract = ethers.Contract;
